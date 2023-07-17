ab08ab21baa79953dec74127462db983bcf0a2f8
Sargis manukyan <sargis.1998@mail.ru>
2023-07-17 19:18:05 +0400

    finall

--- script.js
+++ script.js
@@ -4,25 +4,20 @@ const cors = require("cors");
 const app = express();
 const port = 8080;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.get("/", (req, res) => {
   console.log(req);
   res.send("Hello, Express!");
   console.log("asdasd");
 });
-// console.log("adsads");
 
 app.post("/", (req, res) => {
   console.log(req.body);
-  // Process the data...
   res.status(200).json({ message: "Data received successfully!" });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
