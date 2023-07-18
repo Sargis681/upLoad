b8d2ec657f20e3c15d6c6205d49279f93f225b5d
Sargis manukyan <sargis.1998@mail.ru>
2023-07-19 00:41:09 +0400

    fixeds

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const dropArea = document.querySelector(".container__cart");
 const listSection = document.querySelector(".container__list-section");
 const listContainer = document.querySelector(".container__list");
-const fileSelector = document.querySelector(
-  ".container__coldrap-file-selector"
-);
-const fileSelectorInput = document.querySelector(
-  ".container__coldrap-file-selector-input"
-);
+const fileSelector = document.querySelector(".container__coldrap-file-selector");
+const fileSelectorInput = document.querySelector(".container__coldrap-file-selector-input");
 let rit = document.querySelector(".container__list-title");
 let files = [];
 
@@ -14,6 +10,7 @@ fileSelector.onclick = () => fileSelectorInput.click();
 
 fileSelectorInput.onchange = () => {
   files = [...fileSelectorInput.files];
+  displayFiles();
   uploadFilesInBatches();
 };
 
@@ -41,14 +38,46 @@ dropArea.ondrop = (e) => {
       }
       return result;
     }, []);
+    displayFiles();
     uploadFilesInBatches();
   } else {
-    files = [...e.dataTransfer.files].filter((file) =>
-      typeValidation(file.type)
-    );
+    files = [...e.dataTransfer.files].filter((file) => typeValidation(file.type));
+    displayFiles();
     uploadFilesInBatches();
   }
 };
+
+function displayFiles() {
+  listSection.style.display = "block";
+  listContainer.innerHTML = ""; // Clear the file list
+
+  files.forEach((file) => {
+    const li = document.createElement("li");
+    li.classList.add("in-prog");
+    li.innerHTML = `
+      <div class="col"></div>
+      <div class="col">
+        <div class="file-name">
+          <div class="name">${file.name}</div>
+          <span>0%</span>
+        </div>
+        <div class="file-progress">
+          <span></span>
+        </div>
+        <div class="file-size">${(file.size / (1024 * 1024)).toFixed(2)} MB</div>
+      </div>
+      <div class="col">
+        <svg xmlns="http://www.w3.org/2000/svg" class="cross" height="20" width="20">
+          <path d="m5.979 14.917-.854-.896 4-4.021-4-4.062.854-.896 4.042 4.062 4-4.062.854.896-4 4.062 4 4.021-.854.896-4-4.063Z"/>
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" class="tick" height="20" width="20">
+          <path d="m8.229 14.438-3.896-3.917 1.438-1.438 2.458 2.459 6-6L15.667 7Z"/>
+        </svg>
+      </div>
+    `;
+    listContainer.appendChild(li);
+  });
+}
 
 function uploadFilesInBatches() {
   const batchSize = 3;
@@ -57,8 +86,6 @@ function uploadFilesInBatches() {
 
   const totalBatches = Math.ceil(files.length / batchSize);
   let currentBatch = 0;
-
-  listSection.style.display = "block";
 
   function uploadBatch() {
     const start = currentBatch * batchSize;
@@ -74,32 +101,20 @@ function uploadFilesInBatches() {
     let hasError = false;
 
     batchFiles.forEach((file) => {
-      const li = document.createElement("li");
+      let li;
+      const fileElements = listContainer.getElementsByClassName("name");
+      for (let i = 0; i < fileElements.length; i++) {
+        if (fileElements[i].textContent === file.name) {
+          li = fileElements[i].closest("li");
+          break;
+        }
+      }
+
+      if (!li) {
+        return; // Skip if corresponding list item not found
+      }
+
       li.classList.add("in-prog");
-      li.innerHTML = `
-        <div class="col"></div>
-        <div class="col">
-          <div class="file-name">
-            <div class="name">${file.name}</div>
-            <span>0%</span>
-          </div>
-          <div class="file-progress">
-            <span></span>
-          </div>
-          <div class="file-size">${(file.size / (1024 * 1024)).toFixed(
-            2
-          )} MB</div>
-        </div>
-        <div class="col">
-          <svg xmlns="http://www.w3.org/2000/svg" class="cross" height="20" width="20">
-            <path d="m5.979 14.917-.854-.896 4-4.021-4-4.062.854-.896 4.042 4.062 4-4.062.854.896-4 4.062 4 4.021-.854.896-4-4.063Z"/>
-          </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" class="tick" height="20" width="20">
-            <path d="m8.229 14.438-3.896-3.917 1.438-1.438 2.458 2.459 6-6L15.667 7Z"/>
-          </svg>
-        </div>
-      `;
-      listContainer.prepend(li);
 
       const http = new XMLHttpRequest();
       const data = new FormData();
@@ -107,21 +122,16 @@ function uploadFilesInBatches() {
 
       http.upload.onprogress = (e) => {
         const percentComplete = (e.loaded / e.total) * 100;
-        li.querySelector(".file-name span").innerHTML =
-          Math.round(percentComplete) + "%";
-        li.querySelector(".file-progress span").style.width =
-          percentComplete + "%";
+        li.querySelector(".file-name span").innerHTML = Math.round(percentComplete) + "%";
+        li.querySelector(".file-progress span").style.width = percentComplete + "%";
       };
 
       http.onload = () => {
         if (http.status === 200) {
           li.classList.add("complete");
-          li.classList.remove("in-prog");
           console.log("File uploaded successfully.");
         } else {
-          console.log(
-            "An error occurred during file upload. Error code: " + http.status
-          );
+          console.log("An error occurred during file upload. Error code: " + http.status);
           hasError = true;
         }
 
