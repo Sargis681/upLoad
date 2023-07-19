b3d70e24660954bfdab6e18db016e66f0ef9a704
Sargis681 <sargis.1998@mail.ru>
2023-07-19 11:56:51 +0400

    fixeds

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const dropArea = document.querySelector(".container__cart");
 const listSection = document.querySelector(".container__list-section");
 const listContainer = document.querySelector(".container__list");
-const fileSelector = document.querySelector(".container__coldrap-file-selector");
-const fileSelectorInput = document.querySelector(".container__coldrap-file-selector-input");
+const fileSelector = document.querySelector(
+  ".container__coldrap-file-selector"
+);
+const fileSelectorInput = document.querySelector(
+  ".container__coldrap-file-selector-input"
+);
 let rit = document.querySelector(".container__list-title");
 let files = [];
 
@@ -11,26 +15,34 @@ fileSelector.onclick = () => fileSelectorInput.click();
 fileSelectorInput.onchange = () => {
   files = [...fileSelectorInput.files];
   displayFiles();
-  uploadFilesInBatches();
+
+  if (!isUploading) {
+    uploadFilesInBatches();
+  }
 };
 
 dropArea.ondragover = (e) => {
   e.preventDefault();
-  [...e.dataTransfer.items].forEach((item) => {
-    if (typeValidation(item.type)) {
-      dropArea.classList.add("drag-over-effect");
-    }
-  });
+  dropArea.classList.add("drag-over-effect");
 };
 
 dropArea.ondragleave = () => {
   dropArea.classList.remove("drag-over-effect");
 };
 
+dropArea.ondragenter = (e) => {
+  e.preventDefault();
+  dropArea.classList.add("drag-over-effect");
+};
+
+dropArea.ondragend = () => {
+  dropArea.classList.remove("drag-over-effect");
+};
+
 dropArea.ondrop = (e) => {
   e.preventDefault();
-
   dropArea.classList.remove("drag-over-effect");
+
   if (e.dataTransfer.items) {
     files = [...e.dataTransfer.items].reduce((result, item) => {
       if (item.kind === "file" && typeValidation(item.type)) {
@@ -39,19 +51,31 @@ dropArea.ondrop = (e) => {
       return result;
     }, []);
     displayFiles();
-    uploadFilesInBatches();
+
+    if (!isUploading) {
+      uploadFilesInBatches();
+    }
   } else {
-    files = [...e.dataTransfer.files].filter((file) => typeValidation(file.type));
+    files = [...e.dataTransfer.files].filter((file) =>
+      typeValidation(file.type)
+    );
     displayFiles();
-    uploadFilesInBatches();
+
+    if (!isUploading) {
+      uploadFilesInBatches();
+    }
   }
 };
 
+let a = 0;
+let isUploading = false;
+
 function displayFiles() {
   listSection.style.display = "block";
-  listContainer.innerHTML = ""; // Clear the file list
+  // listContainer.innerHTML = "";
 
   files.forEach((file) => {
+    a = a + 1;
     const li = document.createElement("li");
     li.classList.add("in-prog");
     li.innerHTML = `
@@ -64,7 +88,9 @@ function displayFiles() {
         <div class="file-progress">
           <span></span>
         </div>
-        <div class="file-size">${(file.size / (1024 * 1024)).toFixed(2)} MB</div>
+        <div class="file-size">${(file.size / (1024 * 1024)).toFixed(
+          2
+        )} MB</div>
       </div>
       <div class="col">
         <svg xmlns="http://www.w3.org/2000/svg" class="cross" height="20" width="20">
@@ -76,10 +102,12 @@ function displayFiles() {
       </div>
     `;
     listContainer.appendChild(li);
+    console.log(a);
   });
 }
 
-function uploadFilesInBatches() {
+async function uploadFilesInBatches() {
+  isUploading = true;
   const batchSize = 3;
   rit.append(files.length + "/");
   console.log(files.length);
@@ -87,51 +115,45 @@ function uploadFilesInBatches() {
   const totalBatches = Math.ceil(files.length / batchSize);
   let currentBatch = 0;
 
-  function uploadBatch() {
+  async function uploadBatch() {
     const start = currentBatch * batchSize;
     const end = Math.min(start + batchSize, files.length);
     const batchFiles = files.slice(start, end);
 
+    console.log(batchFiles);
+    console.log(start);
     if (batchFiles.length === 0) {
       console.log("All files uploaded successfully.");
+      isUploading = false;
       return;
     }
 
     let completedCount = 0;
     let hasError = false;
 
-    batchFiles.forEach((file) => {
-      let li;
-      const fileElements = listContainer.getElementsByClassName("name");
-      for (let i = 0; i < fileElements.length; i++) {
-        if (fileElements[i].textContent === file.name) {
-          li = fileElements[i].closest("li");
-          break;
+    await Promise.all(
+      batchFiles.map(async (file) => {
+        console.log("12");
+        let li;
+        const fileElements = listContainer.getElementsByClassName("name");
+        for (let i = 0; i < fileElements.length; i++) {
+          if (fileElements[i].textContent === file.name) {
+            li = fileElements[i].closest("li");
+            break;
+          }
         }
-      }
 
-      if (!li) {
-        return; // Skip if corresponding list item not found
-      }
+        if (!li) {
+          return;
+        }
 
-      li.classList.add("in-prog");
+        li.classList.add("in-prog");
 
-      const http = new XMLHttpRequest();
-      const data = new FormData();
-      data.append("file", file);
-
-      http.upload.onprogress = (e) => {
-        const percentComplete = (e.loaded / e.total) * 100;
-        li.querySelector(".file-name span").innerHTML = Math.round(percentComplete) + "%";
-        li.querySelector(".file-progress span").style.width = percentComplete + "%";
-      };
-
-      http.onload = () => {
-        if (http.status === 200) {
-          li.classList.add("complete");
+        try {
+          await uploadFile(file, li);
           console.log("File uploaded successfully.");
-        } else {
-          console.log("An error occurred during file upload. Error code: " + http.status);
+        } catch (error) {
+          console.log("An error occurred during file upload. Error:", error);
           hasError = true;
         }
 
@@ -147,39 +169,59 @@ function uploadFilesInBatches() {
               uploadBatch();
             } else {
               console.log("All files uploaded successfully.");
+              isUploading = false;
             }
           }
         }
-      };
-
-      http.onerror = () => {
-        console.log("An error occurred during file upload.");
-        hasError = true;
-        completedCount++;
-
-        if (completedCount === batchFiles.length) {
-          console.log("Batch upload failed. Retrying...");
-          uploadBatch();
-        }
-      };
-
-      const serverEndpoint = "http://localhost:8080";
-
-      http.open("POST", serverEndpoint, true);
-
-      li.querySelector(".cross").onclick = () => {
-        http.abort();
-        li.remove();
-        completedCount++;
-
-        if (completedCount === batchFiles.length) {
-          console.log("Batch upload aborted.");
-        }
-      };
-
-      http.send(data);
-    });
+      })
+    );
   }
 
   uploadBatch();
+}
+
+function uploadFile(file, li) {
+  return new Promise((resolve, reject) => {
+    const http = new XMLHttpRequest();
+    const data = new FormData();
+    data.append("file", file);
+
+    http.upload.onprogress = (e) => {
+      const percentComplete = (e.loaded / e.total) * 100;
+      li.querySelector(".file-name span").innerHTML =
+        Math.round(percentComplete) + "%";
+      li.querySelector(".file-progress span").style.width =
+        percentComplete + "%";
+    };
+
+    http.onload = () => {
+      if (http.status === 200) {
+        li.classList.add("complete");
+        resolve();
+      } else {
+        reject(new Error(`File upload failed with status: ${http.status}`));
+      }
+    };
+
+    http.onerror = () => {
+      reject(new Error("An error occurred during file upload."));
+    };
+
+    const serverEndpoint = "http://localhost:8080";
+    http.open("POST", serverEndpoint, true);
+
+    li.querySelector(".cross").onclick = () => {
+      http.abort();
+      li.remove();
+      reject(new Error("File upload aborted."));
+    };
+
+    http.send(data);
+  });
+}
+
+function typeValidation(fileType) {
+  // Replace with your own file type validation logic
+  // Example: Only allow image files
+  return fileType.startsWith("image/");
 }
